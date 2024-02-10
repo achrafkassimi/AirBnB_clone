3d228a5a28fc749130cfa11938ebb3a149f7ef28
achrafkassimi <achraf.kassimi.1995@gmail.com>
2024-02-10 18:17:04 +0100

    ds

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -120,9 +120,10 @@ class TestBaseModel_save(unittest.TestCase):
         first_updated_at = bm.updated_at
         bm.save()
         second_updated_at = bm.updated_at
+        bm.save()
         self.assertLess(first_updated_at, second_updated_at)
         sleep(0.05)
-        bm.save()
+        
         self.assertLess(second_updated_at, bm.updated_at)
 
     def test_save_with_arg(self):
