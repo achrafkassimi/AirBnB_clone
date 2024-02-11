a1c5d623b3e4560c0af6e08f46cba4beb4f66f00
achrafkassimi <achraf.kassimi.1995@gmail.com>
2024-02-11 01:42:00 +0100

    city

--- models/city.py
+++ models/city.py
@@ -8,15 +8,16 @@ from models.base_model import BaseModel
 class City(BaseModel):
     """
     Represent a city.
-
     Attributes:
-        state_id (str): The state id.
-        name (str): The name of the city.
+    state_id (str): The state id.
+    name (str): The name of the city
     """
 
     state_id = ""
     name = ""
 
     def __init__(self, *args, **kwargs):
-        """initializes City"""
+        """
+        initializes City
+        """
         super().__init__(*args, **kwargs)
